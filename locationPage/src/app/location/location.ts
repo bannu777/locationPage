b955d63664ffb0d1c2e93d7ecad1c2a745c04d72
Gopinath Gadipudi <gopinath.gadipudi@hitachids.com>
2026-03-26 12:22:17 +0530

    changed to one component structure

--- locationPage/src/app/location/location.ts
+++ locationPage/src/app/location/location.ts
@@ -1,36 +1,20 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { LocationService } from '../services/location';
-import { Navbar } from '../navbar/navbar';
-import { LocationDetails } from '../location-details/location-details';
-import { LocationContacts } from '../location-contacts/location-contacts';
-import { Hierarchy } from '../hierarchy/hierarchy';
-import { RegionContacts } from '../region-contacts/region-contacts';
-import { Notes } from '../notes/notes';
-import { DistrictContacts } from '../district-contacts/district-contacts';
-import { AreaContacts } from '../area-contacts/area-contacts';
+
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-location',
   standalone : true,
-  imports: [CommonModule,
-    Navbar,
-    LocationDetails,
-    Notes,
-    DistrictContacts,
-    AreaContacts,
-    LocationContacts,
-    Hierarchy,
-    RegionContacts
-  ],
+  imports: [CommonModule],
   templateUrl: './location.html',
   styleUrl: './location.css',
 })
 export class Location implements OnInit{
-locationDetails: any;
-  hierarchy: any;
+locationDetails: any = {};
+  hierarchy: any = {};
 
-  activeTab: string = 'location';  
+  activeTab: any = 'location';  
 
 
 constructor(
--- locationPage/src/app/services/location.spec.ts
+++ locationPage/src/app/services/location.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Location } from './location';
+
 
 describe('Location', () => {
   let service: Location;
